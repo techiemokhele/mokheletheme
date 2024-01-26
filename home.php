00e6af0ebc51144c357779dcd68f444a7b74691b
techiemokhele <neomokhele23@gmail.com>
2024-01-26 15:57:10 +0200

    page update

--- home.php
+++ home.php
@@ -40,7 +40,7 @@
                 <div class="row row-cols-1 row-cols-md-3 g-4 mx-5">
                     <!-- Product 1 -->
                     <div class="col">
-                        <div class="card bg-dark text-light text-center">
+                        <div class="card text-light text-center" style="background-color: #2F3B45; ">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/p1.png"
                                 class="card-img-top mx-auto my-3" style="height: 5vh" alt="Product 1">
                         </div>
@@ -48,7 +48,7 @@
 
                     <!-- Product 2 -->
                     <div class="col">
-                        <div class="card bg-dark text-light text-center">
+                        <div class="card text-light text-center" style="background-color: #2E556F; ">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/p2.png"
                                 class="card-img-top mx-auto my-3" style="height: 5vh" alt="Product 2">
                         </div>
@@ -56,15 +56,15 @@
 
                     <!-- Product 3 -->
                     <div class="col">
-                        <div class="card bg-dark text-light text-center">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/p3.png"
+                        <div class="card text-light text-center" style="background-color: #2E556F; ">
+                            <img src=" <?php echo get_template_directory_uri(); ?>/assets/img/products/p3.png"
                                 class="card-img-top mx-auto my-3" style="height: 5vh" alt="Product 3">
                         </div>
                     </div>
 
                     <!-- Product 4 -->
                     <div class="col">
-                        <div class="card bg-dark text-light text-center">
+                        <div class="card text-light text-center" style="background-color: #2F3B45; ">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/p4.png"
                                 class="card-img-top mx-auto my-3" style="height: 5vh" alt="Product 4">
                         </div>
@@ -72,7 +72,7 @@
 
                     <!-- Product 5 -->
                     <div class="col">
-                        <div class="card bg-dark text-light text-center">
+                        <div class="card text-light text-center" style="background-color: #2F3B45; ">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/p5.png"
                                 class="card-img-top mx-auto my-3" style="height: 5vh" alt="Product 5">
                         </div>
@@ -80,7 +80,7 @@
 
                     <!-- Product 6 -->
                     <div class="col">
-                        <div class="card bg-dark text-light text-center">
+                        <div class="card text-light text-center" style="background-color: #2E556F; ">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/p6.png"
                                 class="card-img-top mx-auto my-3" style="height: 5vh" alt="Product 6">
                         </div>
@@ -91,10 +91,158 @@
         </div>
 
         <!-- services section -->
+        <div class="px-5 my-5 py-3">
+            <h5 class="text-center">SERVICES</h5>
+
+            <div class="container mt-4">
+                <div class="row row-cols-1 row-cols-md-3 g-4 mx-5">
+                    <!-- service 1 -->
+                    <div class="col">
+                        <div class="card text-light text-center"
+                            style="background: #2F3B45; transition: background 0.3s;"
+                            onmouseover="this.style.background='linear-gradient(135deg, #005A96, #359EBD)'"
+                            onmouseout="this.style.background='#2F3B45'">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/Icon.png"
+                                class="card-img-top mx-auto my-3" style="height: 20px; width: 20px;" alt="icon">
+                            <div class="card-body">
+                                <p class="card-title" style="font-size: 12px;">Cloud Security</p>
+                                <p class="card-text" style="font-size: 8px; color: rgba(255, 255, 255, 0.4);">Enim nec
+                                    rhoncus volutpat nullam eros
+                                    sapien pharetra.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- service 2 -->
+                    <div class="col">
+                        <div class="card text-light text-center"
+                            style="background: #2F3B45; transition: background 0.3s;"
+                            onmouseover="this.style.background='linear-gradient(135deg, #005A96, #359EBD)'"
+                            onmouseout="this.style.background='#2F3B45'">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/Icon.png"
+                                class="card-img-top mx-auto my-3" style="height: 20px; width: 20px;" alt="icon">
+                            <div class="card-body">
+                                <p class="card-title" style="font-size: 12px;">Identity Management</p>
+                                <p class="card-text" style="font-size: 8px; color: rgba(255, 255, 255, 0.4);">Enim nec
+                                    rhoncus volutpat nullam eros
+                                    sapien pharetra.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- service 3 -->
+                    <div class="col">
+                        <div class="card text-light text-center"
+                            style="background: #2F3B45; transition: background 0.3s;"
+                            onmouseover="this.style.background='linear-gradient(135deg, #005A96, #359EBD)'"
+                            onmouseout="this.style.background='#2F3B45'">
+                            <img src=" <?php echo get_template_directory_uri(); ?>/assets/img/products/Icon.png"
+                                class="card-img-top mx-auto my-3" style="height: 20px; width: 20px;" alt="icon">
+                            <div class="card-body">
+                                <p class="card-title" style="font-size: 12px;">Strong Authentication</p>
+                                <p class="card-text" style="font-size: 8px; color: rgba(255, 255, 255, 0.4);">Enim nec
+                                    rhoncus volutpat nullam eros
+                                    sapien pharetra.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- service 4 -->
+                    <div class="col">
+                        <div class="card text-light text-center"
+                            style="background: #2F3B45; transition: background 0.3s;"
+                            onmouseover="this.style.background='linear-gradient(135deg, #005A96, #359EBD)'"
+                            onmouseout="this.style.background='#2F3B45'">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/Icon.png"
+                                class="card-img-top mx-auto my-3" style="height: 20px; width: 20px;" alt="icon">
+                            <div class="card-body">
+                                <p class="card-title" style="font-size: 12px;">Public Key Innfrastructure</p>
+                                <p class="card-text" style="font-size: 8px; color: rgba(255, 255, 255, 0.4);">Enim nec
+                                    rhoncus volutpat nullam eros
+                                    sapien pharetra.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- service 5 -->
+                    <div class="col">
+                        <div class="card text-light text-center"
+                            style="background: #2F3B45; transition: background 0.3s;"
+                            onmouseover="this.style.background='linear-gradient(135deg, #005A96, #359EBD)'"
+                            onmouseout="this.style.background='#2F3B45'">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/products/Icon.png"
+                                class="card-img-top mx-auto my-3" style="height: 20px; width: 20px;" alt="icon">
+                            <div class="card-body">
+                                <p class="card-title" style="font-size: 12px;">Workshop and Training</p>
+                                <p class="card-text" style="font-size: 8px; color: rgba(255, 255, 255, 0.4);">Enim nec
+                                    rhoncus volutpat nullam eros
+                                    sapien pharetra.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <!-- offices section -->
+        <div class="px-5 my-5 py-3">
+            <h5 class="text-center">OUR OFFICES</h5>
+
+            <div class="container mx-2">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Frame.png"
+                    class="card-img-top mx-auto my-3" style="height: 50vh;" alt="location">
+            </div>
+        </div>
 
         <!-- shop section -->
+        <div class="px-5 my-5 py-3" style="background: #1A7FA8; border-radius: 25px">
+            <h5 class="text-center">Shop Yubikeys</h5>
+
+            <div class="container mt-5">
+                <div class="row row-cols-1 row-cols-md-3 g-10 mx-1">
+                    <div class="col">
+                        <div class="text-light d-flex flex-column justify-content-center align-items-start"
+                            style="background: #1A7FA8; height: 40vh">
+                            <p class=" card-title" style="font-size: 12px;">Introduction Heading - Our USP</p>
+                            <p class="card-text" style="font-size: 8px; color: rgba(255, 255, 255, 0.4);">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto distinctio impedit
+                                dolores hic porro aperiam recusandae eius, mollitia molestiae tenetur dolorem assumenda
+                                fugit voluptates molestias blanditiis nobis, perspiciatis nemo earum. Lorem ipsum dolor
+                                sit amet consectetur adipisicing elit. Molestias reprehenderit id vel dolores, quae
+                                dolorum eveniet! A modi aut hic tenetur dolorum itaque deleniti laborum incidunt, ullam
+                                amet. Repellendus, quibusdam.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="text-light text-center" style="background: #1A7FA8; ">
+                            <img src=" <?php echo get_template_directory_uri(); ?>/assets/img/security.png"
+                                class="card-img-top mx-auto my-3" style="height: 30vh; width: 30vh" alt="icon">
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="text-light d-flex flex-column justify-content-center align-items-start"
+                            style="background: #1A7FA8; height: 40vh">
+                            <p class="card-title" style="font-size: 12px;">Introduction Heading - Our USP</p>
+                            <p class="card-text" style="font-size: 8px; color: rgba(255, 255, 255, 0.4);">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto distinctio impedit
+                                dolores hic porro aperiam recusandae eius, mollitia molestiae tenetur dolorem assumenda
+                                fugit voluptates molestias blanditiis nobis, perspiciatis nemo earum. Lorem ipsum dolor
+                                sit amet consectetur adipisicing elit. Molestias reprehenderit id vel dolores, quae
+                                dolorum eveniet! A modi aut hic tenetur dolorum itaque deleniti laborum incidunt, ullam
+                                amet. Repellendus, quibusdam.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="justify-content-center text-center mt-2">
+                    <button type="button" class="btn btn-dark">Go to Shop</button>
+                </div>
+            </div>
+        </div>
 
         <!-- reseller section -->
     </div>
